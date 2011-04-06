834c756491a56ae03088398da9be832c2eb99f14
Christian Autermann <christian@autermann.org>
2011-04-06 18:26:18 +0000

    Cleanup

--- src/main/java/de/ifgi/airbase/feeder/io/sos/http/TransactionalSosClient.java
+++ src/main/java/de/ifgi/airbase/feeder/io/sos/http/TransactionalSosClient.java
@@ -10,7 +10,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 
