145448645bb0604c3e7213c53fec28cbd90cd8c3
Christian Autermann <christian@autermann.org>
2013-03-26 10:18:48 +0100

    serialVersionUID

--- src/main/java/de/ifgi/airbase/feeder/io/sos/http/RequestFailedException.java
+++ src/main/java/de/ifgi/airbase/feeder/io/sos/http/RequestFailedException.java
@@ -9,6 +9,7 @@ package de.ifgi.airbase.feeder.io.sos.http;
  * @author Christian Autermann <c.autermann@52north.org>
  */
 public class RequestFailedException extends Exception {
+    private static final long serialVersionUID = 6255358204173302981L;
 
     public RequestFailedException(String message) {
         super(message);
