767d3a30ad48394d744505840f4c5f6e3200e145
Christian Autermann <christian@autermann.org>
2013-03-26 10:15:01 +0100

    member order

--- src/main/java/de/ifgi/airbase/feeder/Feeder.java
+++ src/main/java/de/ifgi/airbase/feeder/Feeder.java
@@ -16,7 +16,6 @@ import de.ifgi.airbase.feeder.io.sos.SosClient;
 import de.ifgi.airbase.feeder.io.sos.http.RequestFailedException;
 import de.ifgi.airbase.feeder.io.zip.Unzipper;
 import de.ifgi.airbase.feeder.util.Utils;
-import java.util.logging.Level;
 
 /**
  * 
@@ -37,41 +36,55 @@ public class Feeder {
 //	private static final String OUTPUT_FILE_PATH = Utils.get(OUTPUT_FILE_PATH_PROPERTY);
 	
 	
-	protected void process(String fileName) {
-		long start = System.currentTimeMillis();
-		try {
-			File f = new File(fileName);
-			if (f.isDirectory()) {
-				for (File f2 : f.listFiles(new FileExtensionFilter("zip"))) {
-					processFile(f2);
-				}
-			} else if (f.isFile()) {
-				processFile(f);
-			} else {
-				throw new Error(fileName + " is neither a directory nor a normal file... what is it?");
-			}
-			File failed = new File(Utils.getFailedRequestPrintPath());
-	        if (failed.exists() && failed.isDirectory()) {
-	        	File[] files = failed.listFiles(new FileExtensionFilter(".xml"));
-	        	if (files.length != 0) {
-	        		log.info("Processing {} failed Requests.", files.length);
-	        		StaticFileFeeder.processDirectory(failed);
-	        		int size = failed.listFiles(new FileExtensionFilter(".xml")).length;
-	        		if (size != 0) {
-	        			log.info("Still {} failed requests in '{}'.",size,failed.getAbsolutePath());
-	        		}
-	        	}
-	        }
-		} catch (Exception e) {
-			log.warn("Unexpected Exception.", e);
-		} finally {
-			log.info("Processed everything in {}.", Utils.timeElapsed(start));
-		}
-	}
+    /**
+     * @param args arguments for {@code Eea2Sos}
+     *
+     * @throws MalformedURLException if given URL is not valid.
+     */
+    public static void main(String[] args) throws MalformedURLException {
+        Feeder eea2sos = new Feeder();
+        if (args.length == 1) {
+            eea2sos.process(args[0]);
+        } else {
+            System.exit(1);
+        }
+    }
+
+    protected void process(String fileName) {
+        long start = System.currentTimeMillis();
+        try {
+            File f = new File(fileName);
+            if (f.isDirectory()) {
+                for (File f2 : f.listFiles(new FileExtensionFilter("zip"))) {
+                    processFile(f2);
+                }
+            } else if (f.isFile()) {
+                processFile(f);
+            } else {
+                throw new Error(fileName + " is neither a directory nor a normal file... what is it?");
+            }
+            File failed = new File(Utils.getFailedRequestPrintPath());
+            if (failed.exists() && failed.isDirectory()) {
+                File[] files = failed.listFiles(new FileExtensionFilter(".xml"));
+                if (files.length != 0) {
+                    log.info("Processing {} failed Requests.", files.length);
+                    StaticFileFeeder.processDirectory(failed);
+                    int size = failed.listFiles(new FileExtensionFilter(".xml")).length;
+                    if (size != 0) {
+                        log.info("Still {} failed requests in '{}'.", size, failed.getAbsolutePath());
+                    }
+                }
+            }
+        } catch (Exception e) {
+            log.warn("Unexpected Exception.", e);
+        } finally {
+            log.info("Processed everything in {}.", Utils.timeElapsed(start));
+        }
+    }
 
 	protected void processFile(File f) throws RequestFailedException {
-		long fileStartTime = System.currentTimeMillis();
-		try {
+        long fileStartTime = System.currentTimeMillis();
+        try {
 //			//write to file
 //			if (IS_FILE_WRITER){
 //				
@@ -96,17 +109,17 @@ public class Feeder {
 //					}
 //				
 //			}
-			
-			//send to SOS-T
+
+            //send to SOS-T
 //			else {
-				SosClient sos = SosClient.newInstance();
-				Unzipper uz = new Unzipper(f);
-				EEAParser p = new EEAParser(uz.getStationsFile(),
-											uz.getRawDataDirectory(), 
-											uz.getStatisticsFile(),
-											uz.getConfigurationFile());
-				for (EEAStation station : p.getStations()) {
-					if (REGISTER_STATIONS) {
+            SosClient sos = SosClient.newInstance();
+            Unzipper uz = new Unzipper(f);
+            EEAParser p = new EEAParser(uz.getStationsFile(),
+                                        uz.getRawDataDirectory(),
+                                        uz.getStatisticsFile(),
+                                        uz.getConfigurationFile());
+            for (EEAStation station : p.getStations()) {
+                if (REGISTER_STATIONS) {
                         try {
                             sos.registerStation(station);
                             if (REGISTER_OBSERVATIONS) {
@@ -121,35 +134,20 @@ public class Feeder {
                         } catch (RequestFailedException ex) {
                             if (EXIT_ON_FAILURE) { throw ex; }
                         }
-					}
-					
-				}
-				sos = null;
-				uz = null;
-				p = null;
-//			}
-		} catch (ZipException e) {
-			log.warn("Unable to unpack zip file.", e);
-		} catch (IOException e) {
-			log.warn("Unexpected IO error.", e);
-		} finally {
-			log.info("Processed {} in {}.", f.getName(),
-					Utils.timeElapsed(fileStartTime));
-		}
-	}
+                }
 
-	/**
-	 * @param args
-	 *            arguments for {@code Eea2Sos}
-	 * @throws MalformedURLException
-	 *             if given URL is not valid.
-	 */
-	public static void main(String[] args) throws MalformedURLException {
-		Feeder eea2sos = new Feeder();
-		if (args.length == 1) {
-			eea2sos.process(args[0]);
-		} else {
-			System.exit(1);
-		}
-	}
+            }
+            sos = null;
+            uz = null;
+            p = null;
+//			}
+        } catch (ZipException e) {
+            log.warn("Unable to unpack zip file.", e);
+        } catch (IOException e) {
+            log.warn("Unexpected IO error.", e);
+        } finally {
+            log.info("Processed {} in {}.", f.getName(),
+                     Utils.timeElapsed(fileStartTime));
+        }
+    }
 }
